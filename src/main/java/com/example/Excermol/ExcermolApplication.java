7fbd3363ba94fee862d42f9f837c433c97f7e3e4
NicatHsynlyv <nicat200507@gmail.com>
2025-08-05 17:17:36 +0800

    Ilk commit: proyekt fayllari elave edildi

---
+++ src/main/java/com/example/Excermol/ExcermolApplication.java
@@ -0,0 +1,13 @@
+package com.example.Excermol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcermolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcermolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Excermol/ExcermolApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.Excermol;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ExcermolApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
