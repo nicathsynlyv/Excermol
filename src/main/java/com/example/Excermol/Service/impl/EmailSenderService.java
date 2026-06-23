81ac59727b3b13ee5009955f5131650a4511d344
NicatHsynlyv <nicat200507@gmail.com>
2026-06-23 12:32:06 +0800

    i added logging to Emailsenderservice.class

--- src/main/java/com/example/Excermol/Service/impl/EmailSenderService.java
+++ src/main/java/com/example/Excermol/Service/impl/EmailSenderService.java
@@ -1,12 +1,14 @@
 package com.example.Excermol.Service.impl;
 
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailSenderService {
     private final JavaMailSender mailSender;
 
@@ -16,19 +18,23 @@ public class EmailSenderService {
 
     // Sadə email göndər
     public void sendSimpleEmail(String to, String subject, String body) {
+        log.info("Sending email to {}", to);
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
         mailSender.send(message);
+        log.info("Email sent successfully");
     }
     // HTML email göndər
     public void sendHtmlEmail(String to, String subject, String htmlBody) throws Exception {
+        log.info("Sending HTML email to {}", to);
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlBody, true); // true = HTML
         mailSender.send(message);
+        log.info("HTML email sent successfully");
     }
 }
