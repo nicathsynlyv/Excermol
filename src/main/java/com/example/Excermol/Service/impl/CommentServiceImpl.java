16e79eb7846c30ac50b4b54b06dc116d68a13982
NicatHsynlyv <nicat200507@gmail.com>
2026-06-23 11:51:56 +0800

    I added logging to CommentServiceimpl class

--- src/main/java/com/example/Excermol/Service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/Excermol/Service/impl/CommentServiceImpl.java
@@ -15,6 +15,7 @@ import com.example.Excermol.repository.CommentRepository;
 import com.example.Excermol.repository.TaskRepository;
 import com.example.Excermol.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -23,6 +24,7 @@ import java.util.stream.Collectors;
 
 @Service
 @Transactional
+@Slf4j
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
@@ -40,61 +42,95 @@ public class CommentServiceImpl implements CommentService {
         this.commentMapper = commentMapper;
     }
 
+    //1
     @Override
     public CommentResponseDTO createComment(CommentCreateRequestDTO dto) {
+        log.info("Creating comment for task id {} ", dto.getTaskId());
         // task tap
         Task task = taskRepository.findById(dto.getTaskId())
-                .orElseThrow(() -> new TaskNotFoundException("Task tapılmadı: " + dto.getTaskId()));
-
+                .orElseThrow(() -> {
+                    log.warn("Task not found with Id: {}", dto.getTaskId());
+                    return new TaskNotFoundException("Task tapılmadı: " + dto.getTaskId());
+                });
         // author tap
         User author = userRepository.findById(dto.getAuthorId())
-                .orElseThrow(() -> new UserNotFoundException("User tapılmadı: " + dto.getAuthorId()));
-
+                .orElseThrow(() -> {
+                    log.warn("User not found with Id: {}", dto.getAuthorId());
+                    return new UserNotFoundException("User tapılmadı: " + dto.getAuthorId());
+                });
         Comment comment = commentMapper.toEntity(dto);
         comment.setTask(task);
         comment.setAuthor(author);
         comment.setCreatedAt(LocalDateTime.now());
 
         Comment saved = commentRepository.save(comment);
+        log.info("comment created successfully with id: {}", saved.getId());
         return commentMapper.toResponseDTO(saved);
     }
 
+    //2
     @Override
     public List<CommentResponseDTO> getCommentsByTaskId(Long taskId) {
-        return commentRepository.findAllByTaskId(taskId)
+        log.info("Fetching comment for task id: {}", taskId);
+
+        List<CommentResponseDTO> comments = commentRepository.findAllByTaskId(taskId)
                 .stream()
                 .map(commentMapper::toResponseDTO)
                 .collect(Collectors.toList());
+        log.info("Retrieved {} comments for task id: {} ", comments.size(), taskId);
+        return comments;
     }
 
+    //3
     @Override
     public CommentResponseDTO getCommentById(Long id) {
+        log.info("Fetching comment with id: {}", id);
         Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new CommentNotFoundException("Comment tapılmadı: " + id));
+                .orElseThrow(() -> {
+                    log.warn("Comment not found with id: {}", id);
+                    return new CommentNotFoundException("Comment tapılmadı: " + id);
+                });
+        log.info("Comment found with id: {}", id);
         return commentMapper.toResponseDTO(comment);
     }
 
+    //4
     @Override
     public CommentResponseDTO updateComment(Long id, CommentUpdateRequestDTO dto) {
+        log.info("Updateing comment with id: {}", id);
         Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new CommentNotFoundException("Comment tapılmadı: " + id));
-
+                .orElseThrow(() -> {
+                    log.warn("Comment not found for update. Id: {}", id);
+                    return new CommentNotFoundException("Comment tapılmadı: " + id);
+                });
         commentMapper.updateEntity(comment, dto);
 
         Comment updated = commentRepository.save(comment);
+        log.info("Comment Updated successfully. Id: {}", id);
         return commentMapper.toResponseDTO(updated);
     }
 
+    //5
     @Override
     public void deleteComment(Long id) {
+        log.info("Deleting comment with id: {}", id);
         Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new CommentNotFoundException("Comment tapılmadı: " + id));
+                .orElseThrow(() -> {
+                    log.warn("Comment not found for deletion. Id: {}", id);
+                   return new CommentNotFoundException("Comment tapılmadı: " + id);
+                });
+
         commentRepository.delete(comment);
+        log.info("Comment deleted successfully. Id: {}", id);
     }
 
+    //6
     @Override
     public Integer getCommentCountByTaskId(Long taskId) {
-        return commentRepository.countByTaskId(taskId);
+        log.info("Fetching comment count for task id: {}", taskId);
+        Integer count =  commentRepository.countByTaskId(taskId);
+        log.info("Fetched  count for task id: {} is {}", taskId, count);
+        return count;
     }
 }
 
