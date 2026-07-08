5a14908de9be3962963c50062c1bf1a0065c8a40
NicatHsynlyv <nicat200507@gmail.com>
2026-07-08 21:04:38 +0800

    i fix  Validation to campaign  entity and campaignrequest dto

--- src/main/java/com/example/Excermol/entity/dtos/CampaignRequestDTO.java
+++ src/main/java/com/example/Excermol/entity/dtos/CampaignRequestDTO.java
@@ -3,15 +3,21 @@ package com.example.Excermol.entity.dtos;
 import com.example.Excermol.enums.CampaignStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CampaignRequestDTO {
+
+
     @NotBlank(message = "Kampaniya adı boş ola bilməz")
+    @Size(min = 2,max = 100,message = "Ad 2-100 simvol arasi olmalidir")
     private String name;
 
+
+    @NotNull(message = "Status boş ola bilməz")
     private CampaignStatus status;
 
 
