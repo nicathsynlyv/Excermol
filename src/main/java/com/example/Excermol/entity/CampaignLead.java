ac42d0138bca99f94dcf1dc8bcdda06f21eafbd4
NicatHsynlyv <nicat200507@gmail.com>
2026-07-10 12:38:14 +0800

    i fix  Validation to campaignlead  entity and campaignleadrequest dto

--- src/main/java/com/example/Excermol/entity/CampaignLead.java
+++ src/main/java/com/example/Excermol/entity/CampaignLead.java
@@ -2,6 +2,8 @@ package com.example.Excermol.entity;
 
 import com.example.Excermol.enums.LeadStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -13,6 +15,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CampaignLead {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,7 +25,15 @@ public class CampaignLead {
     @JoinColumn(name = "campaign_id")
     private Campaign campaign;
 
+    @NotBlank
+    @Size(min = 2,max = 100)
+    @Column(nullable = false, length = 100)
     private String leadName;       // Məs: "Anwar Hussen"
+
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    @Column(nullable = false, length = 100)
     private String leadEmail;      // "anwar380@gmail.com"
 
     // Şirkətlə əlaqə (Company entity-n varsa)
@@ -35,9 +46,11 @@ public class CampaignLead {
     private LocalDate lastActive;  // Son aktivlik
 
     // Proqress (0 - 100%)
+    @Min(0) @Max(100)
     private Integer progress;
 
-
+    @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private LeadStatus status;
 }
--- src/main/java/com/example/Excermol/entity/dtos/CampaignLeadRequestDTO.java
+++ src/main/java/com/example/Excermol/entity/dtos/CampaignLeadRequestDTO.java
@@ -1,6 +1,7 @@
 package com.example.Excermol.entity.dtos;
 
 import com.example.Excermol.enums.LeadStatus;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +10,26 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class CampaignLeadRequestDTO {
+
+    @NotBlank(message = "Lead adı boş ola bilməz")
+    @Size(min = 2, max = 100, message = "Ad 2-100 simvol arası olmalıdır")
     private String leadName;
+
+    @NotBlank(message = "Lead email boş ola bilməz")
+    @Email(message = "Email formatı düzgün deyil")
+    @Size(max = 100, message = "Email 100 simvoldan çox ola bilməz")
     private String leadEmail;
+
     private Long companyId;
+
     private Long campaignId;
+
     private LocalDate date;
+
+    @Min(value = 0, message = "Progress mənfi ola bilməz")
+    @Max(value = 100, message = "Progress 100-dən çox ola bilməz")
     private Integer progress;
+
+    @NotNull(message = "Status boş ola bilməz")
     private LeadStatus status;
 }
