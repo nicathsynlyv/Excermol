90815f27e21c2e091c3fc926d363c16373608672
NicatHsynlyv <nicat200507@gmail.com>
2026-07-20 03:54:58 +0800

    i hidden Database connection information

--- src/test/java/com/example/Excermol/PasswordEncoderTest.java
+++ src/test/java/com/example/Excermol/PasswordEncoderTest.java
@@ -1,17 +1,17 @@
-//package com.example.Excermol;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//public class PasswordEncoderTest {
-//    @Test
-//    void encodePasswords() {
-//
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//
-//        System.out.println("Kamal2026! -> " + encoder.encode("Kamal2026!"));
-//    }
-//}
+package com.example.Excermol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest {
+    @Test
+    void encodePasswords() {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        System.out.println("Lera123 -> " + encoder.encode("Lera123"));
+    }
+}
 
 
 //bu test classi evvel test ucun yazdigim userlerin passwordunu bcrypt formasina kecirmeye komek edir
