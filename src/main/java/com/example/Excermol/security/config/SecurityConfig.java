6de524ab6a19066d3a445a02fc0be9e105806fb2
NicatHsynlyv <nicat200507@gmail.com>
2026-08-07 12:38:24 +0400

    Add robots.txt

--- src/main/java/com/example/Excermol/security/config/SecurityConfig.java
+++ src/main/java/com/example/Excermol/security/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
                         .accessDeniedHandler(customAccessDeniedHandler)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/index.html","/*.ico","/*.png").permitAll()
+                        .requestMatchers("/","/index.html","/*.ico","/*.png","/robots.txt").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/oauth2/**","/login/oauth2/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
