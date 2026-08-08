e6ec1082644ede7f6812c0ed8b28d0388f33c8e4
NicatHsynlyv <nicat200507@gmail.com>
2026-08-08 06:47:21 +0400

    i added task entity explanation

--- src/main/java/com/example/Excermol/entity/Task.java
+++ src/main/java/com/example/Excermol/entity/Task.java
@@ -17,7 +17,8 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Task {
-    @Id
+
+    @Id  //primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -26,31 +27,36 @@ public class Task {
     @Column(nullable = false, length = 100)
     private String title;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT")  //TEXT PostgreSQL-də uzun mətn saxlamaq üçün istifadə olunur
     private String description;
 
-    private LocalDate dueDate;
+    private LocalDate dueDate;  //Task-ın son tarixidir
+
     // create ve update ayri ayri
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+
+//    @PrePersist entity database-ə ilk dəfə yazılmamışdan əvvəl işləyir.
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
 
+
+//    Entity database-də update olunmazdan əvvəl çağırılır.
     @PreUpdate
     protected void onUpdate() {
         this.updatedAt = LocalDateTime.now();
     }
 
-    @NotNull
-    @Enumerated(EnumType.STRING)
+    @NotNull  //null ola bilmez
+    @Enumerated(EnumType.STRING) //EnumType.STRING istifadə edirəm ki, enum dəyərləri database-də öz adları ilə saxlanılsın və ordinal dəyişikliklərindən yaranan problemlərdən qaçım
     private TaskPriority priority;
 
     // String lead → Person-a dəyişdirildi
-    @ManyToOne
+    @ManyToOne //Bir çox task eyni Person-a aid ola bilər
     @JoinColumn(name = "lead_id")
     private Person lead;
 
@@ -58,7 +64,7 @@ public class Task {
     @Min(0)
     private int progress; // 0-100%
 
-    // Kanban sütununda sıra üçün
+    // Kanban board-da task-ların sırasını saxlamaq üçün istifadə olunur
     private Integer sortOrder;
 
     // UI-da "02/43" kimi göstərilir
@@ -70,7 +76,7 @@ public class Task {
     @Enumerated(EnumType.STRING)
     private TaskStatus status; // TODO, IN_PROGRESS, DONE
 
-    // Bir task-ın çoxlu "tag"ı ola bilər
+    // Bir task-ın çoxlu "tag"ı ola bilər,bir taga ait bir neçə task ola bilər
     @ManyToMany
     @JoinTable(
             name = "task_tags",
@@ -79,7 +85,7 @@ public class Task {
     )
     private Set<Tag> tags;
 
-    // Bir task-ın bir neçə user-i ola bilər
+    // Bir task-ın bir neçə user-i ola bilər,və həmçinin bir userin bir neçə taskı ola bilər
     @ManyToMany
     @JoinTable(
             name = "task_users",
@@ -90,6 +96,9 @@ public class Task {
 
 
     //comments ile elaqe
+    // cascade-Bu isə Task üzərində edilən bəzi əməliyyatların əlaqəli Comment-lərə də ötürülməsini təmin edir.
+    // Məsələn Task silinəndə onun comment-lərinin də silinməsi üçün istifadə olunur
+    // orphanRemoval = true ,Əlaqədən çıxarılmış child entity artıq parent-a aid deyilsə, database-dən də silinə bilər.
     @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Comment> comments;
 
